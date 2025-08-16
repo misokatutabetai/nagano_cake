bacf77540414cf5fb8fe2fc255f3380d1d3a7de1
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-08-16 09:31:23 +0000

    Add Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,49 @@
 Rails.application.routes.draw do
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  scope module: :public do
+    root to: "homes#top"
+    get :about, to: "homes#about"
+    resources :items, only: [:index, :show]
+
+    scope :customers do
+      get :my_page, to: "customers#show"
+      scope :infomation do
+        get "edit", to: "customers#edit", as: "update_customer"
+        patch root to: "customers#update", as: "edit_customer"
+      end
+      get "unsubcribe", to: "customers#unsubcribe", as: "unsubcribe"
+      patch "withdraw", to: "customers#withdraw", as: "withdraw"
+    end
+    resources :cart_items, only: [:index, :update, :destroy, :create] do
+      member do
+        delete :destroy_all
+      end
+    end
+    resources :orders, only: [:new, :index, :create, :show]
+    scope :orders do
+      get :confirm, to: "orders#confirm"
+      get :thanks, to: "orders#thanks"
+    end
+    resources :address, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :items
+    resources :generes, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :create, :edit, :update]
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: :update
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
