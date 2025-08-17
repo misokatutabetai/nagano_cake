aacda4ae2e816b11619c04599090a360ec3ecf13
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-08-17 11:35:44 +0000

    Add Item index/new/create/show/edit/update/destroy

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,45 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
   def index
+    @items = Item.all
   end
 
   def new
+    @genres = Genre.all
+    @item = Item.new
   end
 
   def create
+    item = Item.create(item_params)
+    redirect_to admin_item_path(item.id)
   end
 
   def show
+    @item = Item.find(params[:id])
+    @tax = 1.1
   end
 
   def edit
+    @genres = Genre.all
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(params[:id])
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to admin_items_path
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :genre_id, :price, :is_active, :image)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :order_details
+
+  has_one_attached :image
+
+  validates :name, presence:true
+  validates :price, presence:true
 end
