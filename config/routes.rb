169a0ab9eb9f4ef458ff65987f071e1e3100b8e0
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-08-17 07:41:24 +0000

    Add Genre index/create/edit/destroy

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "homes#top"
     resources :items
-    resources :genres, only: [:index, :create, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
     resources :customers, only: [:index, :create, :edit, :update]
     resources :orders, only: [:index, :show, :update] do
       resources :order_details, only: :update
