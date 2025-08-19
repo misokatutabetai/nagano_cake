1783fd4deda3b9158e8b4fe6f1fe9e0abd65a2d7
kuua0119 <219413026+kuua0119@users.noreply.github.com>
2025-08-19 13:52:25 +0000

    Add customer  login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     case resource
@@ -22,4 +23,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :password, :password_confirmation, :postal_code, :address, :telephone_number])
+  end
 end
