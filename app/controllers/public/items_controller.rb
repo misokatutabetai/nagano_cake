7a8036f6158c8d706d589674c0ff55b83fa0c174
kuua0119 <219413026+kuua0119@users.noreply.github.com>
2025-08-21 13:10:17 +0000

    Add feature-top-page

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,18 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if item.save
+      redirect_to cart_items_path
+    end
+      render :new
+  end 
 end
